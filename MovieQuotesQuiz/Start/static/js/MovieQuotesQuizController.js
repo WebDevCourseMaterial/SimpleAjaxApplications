dd06fb00c0c0b4b9a9fd7f53fe8d1689860e3e3b
Dave Fisher <fisherds@rose-hulman.edu>
2014-11-05 00:40:18 -0500

    Remove ajax actions
    Perhaps this is the only content I will make them implement???

--- MovieQuotesQuiz/Start/static/js/MovieQuotesQuizController.js
+++ MovieQuotesQuiz/Start/static/js/MovieQuotesQuizController.js
@@ -28,21 +28,9 @@ rh.mq.MovieQuotesQuizController = function() {
 
 
 rh.mq.MovieQuotesQuizController.prototype.fetchQuestions = function() {
-  // $.get("/quizquestions?questions=" + questionPerRound, function(data, status) {
-  // console.log("Data: " + data.questions + "\nStatus: " + status);
-  // });
-  var movieQuotesQuizController = this;
   var questionPerRound = parseInt($(".dropdown-menu a.active").text());
+  // TODO: Implement
   
-  $.getJSON("/quizquestions", {"questions": questionPerRound})
-  .done(function(json) {
-    console.log("JSON Data: " + JSON.stringify(json));
-    movieQuotesQuizController.quizQuestionController.displayNewQuestions(json.questions);
-    movieQuotesQuizController.quizStatController.newRound(questionPerRound);
-  }).fail(function(jqxhr, textStatus, error) {
-    var err = textStatus + ", " + error;
-    console.log("Request Failed: " + err);
-  });
 };
 
 
@@ -72,26 +60,12 @@ rh.mq.MovieQuotesQuizController.prototype.enableButtons = function() {
   });
   
   $("#show-add-modal").click(function() {
-
     $("#movie-input").val("");
     $("#quote-input").val("");
   });
   
   $("#add-quote-button").click(function() {
-    var movie = $("#movie-input").val();
-    var quote = $("#quote-input").val();
-    var moviequote = {"quote": quote, "movie": movie, "api": "json"};
-    console.log("YOU clicked on add quote with " + JSON.stringify(moviequote));
-    
- // Send the data using post
-    $.post( "/insertquote", moviequote )
-    .done(function( data ) {
-      console.log("Successfully added " + JSON.stringify(data));
-    })
-    .fail(function(jqxhr, textStatus, error) {
-      var err = textStatus + ", " + error;
-      console.log("POST Request Failed: " + err);
-    });
+    // TODO: Implement
     
   });
 };
