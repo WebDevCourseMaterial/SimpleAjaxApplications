f7cb47e48f5eb5c5beb2b90974e1cba917bde1e9
Dave Fisher <fisherds@rose-hulman.edu>
2014-11-09 22:54:55 -0500

    Remove getJSON solution

--- MovieQuotesQuiz/static/js/MovieQuotesQuizController.js
+++ MovieQuotesQuiz/static/js/MovieQuotesQuizController.js
@@ -42,17 +42,6 @@ rh.mq.MovieQuotesQuizController.prototype.fetchQuestions = function() {
   //  - displayNewQuestions with the quizQuestionController using the questions array.
   //  - create a newRound with the quizStatsController
   //  - scroll the window to the top
-  
-  $.getJSON("/quizquestions", {"questions": questionPerRound})
-  .done(function(json) {
-    console.log("JSON Data: " + JSON.stringify(json));
-    movieQuotesQuizController.quizQuestionController.displayNewQuestions(json.questions);
-    movieQuotesQuizController.quizStatController.newRound(questionPerRound);
-    $(window).scrollTop(0);
-  }).fail(function(jqxhr, textStatus, error) {
-    var err = textStatus + ", " + error;
-    console.log("Request Failed: " + err);
-  });
 };
 
 
@@ -94,7 +83,7 @@ rh.mq.MovieQuotesQuizController.prototype.enableButtons = function() {
     var movie = $("#movie-input").val();
     var quote = $("#quote-input").val();
     var moviequote = {"quote": quote, "movie": movie, "api": "json"};
-    console.log("YOU clicked on add quote with " + JSON.stringify(moviequote));
+    console.log("You clicked on add quote with " + JSON.stringify(moviequote));
     
     // TODO: Send the data using an AJAX post.
     // When complete simply log the response nothing more.
