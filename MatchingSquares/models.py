da78d33986446936f2cc1c3892e70037e0bc5193
Dave Fisher <fisherds@rose-hulman.edu>
2014-11-16 21:55:28 -0500

    Remove the starting code for the matching game plan

--- MatchingSquares/models.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.ext import ndb
-
-class Weatherpic(ndb.Model):
-    image_url = ndb.StringProperty()
-    caption = ndb.StringProperty()
-    last_touch_date_time = ndb.DateTimeProperty(auto_now=True)
